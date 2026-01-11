100fb28c5b34062bbaa7275897eba87e00d720c2
Alvaro Calderon <alvaro.calderon@pucp.edu.pe>
2026-01-10 23:48:01 -0500

    fix dto

--- src/main/java/com/tupack/palletsortingapi/order/infrastructure/outbound/dto/PendingOrderDTO.java
+++ src/main/java/com/tupack/palletsortingapi/order/infrastructure/outbound/dto/PendingOrderDTO.java
@@ -1,21 +1,26 @@
 package com.tupack.palletsortingapi.order.infrastructure.outbound.dto;
-}
-    private String orderStatus;
-    private LocalDate pickupDate;
-    private String toAddress;
-    private String fromAddress;
-    private String clientName;
-    private String id;
-public class PendingOrderDTO {
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import lombok.NoArgsConstructor;
 import lombok.Data;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PendingOrderDTO {
+  private String id;
+  private String orderStatus;
+  private LocalDate pickupDate;
+  private String toAddress;
+  private String fromAddress;
+  private String clientName;
+}
 
 
